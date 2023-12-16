f3b4d8de5f3e8943765f2b852c0f1cae70579297
Dvirus <bdvirus@gmail.com>
2023-12-17 00:10:10 +0200

    upload files
    upload files first edition

---
+++ uploadfiles/dist/runtime/translations/default.ts
@@ -0,0 +1,16 @@
+export default {
+  addShape: 'Add Shape: ',
+  loadingFile: 'Loading file: ',
+  loading: 'Loading',
+  shape: 'shape...',
+  successfullyFinishedTheProcess: 'Successfully finished the proccess!',
+  successfullyAddedShapeToTheMap: 'Successfully added shape to the map.',
+  successfullyLoadedPolygonShape: 'Successfully loaded polygon shape.',
+  successfullyLoadedPointShape: 'Successfully loaded point shape.',
+  successfullyLoadedPolylineShape: 'Successfully loaded polyline shape.',
+  submitJobNotification: 'request has submited for: ',
+  waitingNotofication: 'waiting for a response for: ',
+  UserInstructions: 'Only zip or dwg files can be uploaded',
+  error: 'error load file',
+  fethingSucceeded: 'Sucessfully finished fetching data for: '
+};
---
+++ uploadfiles/dist/setting/translations/default.ts
@@ -0,0 +1,6 @@
+export default {
+  selectMapWidget: "Select Map Widget:",
+  generateToolURL: "Shape generation Tool URL: ",
+  DWGToolURL: "DWG uploading Tool URL: ",
+  GPToolExecute: "DWG Execution Tool URL: "
+};
